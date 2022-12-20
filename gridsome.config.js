92ce958d83be06455046f63e7759915039b4f050
Maxwell Nderitu <nderitumaxwell@gmail.com>
2022-12-20 19:54:26 +0300

    Reverting to gridsome

---
+++ gridsome.config.js
@@ -0,0 +1,41 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here requires a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Blog with Stories',
+  siteDescription: 'Thoughts, stories and ideas. I know original',
+
+  templates: {
+    Post: '/:title',
+    Tag: '/tag/:id'
+  },
+
+  plugins: [
+    {
+      use: '@gridsome/source-ghost',
+      options: {
+        baseUrl: 'https://simple-idea.ghost.io',
+        contentKey: 'bfea7d2ded2a625180005b0b93',
+        routes: {
+          post: '/:slug',
+          page: '/:slug'
+        }
+      }
+    }
+  ],
+
+  transformers: {
+    //Add markdown support to all file-system sources
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        '@gridsome/remark-prismjs'
+      ]
+    }
+  }
+}
