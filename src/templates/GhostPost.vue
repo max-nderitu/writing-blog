a073eb946de904aba96a7ec2ee0f052b115c483c
Maxwell Nderitu <nderitumaxwell@gmail.com>
2023-04-16 14:47:26 +0300

    misc: enabling & fixing remark configurations

--- src/templates/GhostPost.vue
+++ src/templates/GhostPost.vue
@@ -1,33 +1,32 @@
 <template>
-  <Layout>
-    <div class="post-title">
-      <h1 class="post-title__text">
-        {{ $page.post.title }}
-      </h1>
+    <Layout>
+        <div class="post-title">
+            <h1 class="post-title__text">
+                {{ $page.post.title }}
+            </h1>
 
-      <PostMeta :post="$page.post" />
+            <PostMeta :post="$page.post"/>
 
-    </div>
+        </div>
 
-    <div class="post content-box">
-      <div class="post__header">
-        <g-image alt="Cover image" v-if="$page.post.cover_image" :src="$page.post.cover_image" />
-      </div>
+        <div class="post content-box">
+            <div class="post__header">
+                <g-image alt="Cover image" v-if="$page.post.cover_image" :src="$page.post.cover_image"/>
+            </div>
 
-      <div class="post__content" v-html="$page.post.content" />
+            <div class="post__content" v-html="$page.post.content"/>
 
-      <div class="post__footer">
-        <PostTags :post="$page.post" />
-      </div>
-    </div>
+            <div class="post__footer">
+                <PostTags :post="$page.post"/>
+            </div>
+        </div>
 
-    <div class="post-comments">
-        <!--        Disable comments for now-->
-        <!--        <div id="remark42"></div>-->
-    </div>
+        <div class="post-comments">
+            <div id="remark42"></div>
+        </div>
 
-    <Author class="post-author" />
-  </Layout>
+        <Author class="post-author"/>
+    </Layout>
 </template>
 
 <script>
@@ -36,22 +35,22 @@ import PostTags from '~/components/PostTags'
 import Author from '~/components/Author.vue'
 
 export default {
-  components: {
-    Author,
-    PostMeta,
-    PostTags
-  },
-  metaInfo () {
-    return {
-      title: this.$page.post.title,
-      meta: [
-        {
-          name: 'description',
-          content: this.$page.post.description
+    components: {
+        Author,
+        PostMeta,
+        PostTags
+    },
+    metaInfo() {
+        return {
+            title: this.$page.post.title,
+            meta: [
+                {
+                    name: 'description',
+                    content: this.$page.post.description
+                }
+            ]
         }
-      ]
     }
-  }
 }
 </script>
 
