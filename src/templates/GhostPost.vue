3e14455edff35259f0586800e5c72725acb49716
Maxwell Nderitu <nderitumaxwell@gmail.com>
2023-04-22 17:49:56 +0300

    fix: true fix remark42 loading issue 😊

--- src/templates/GhostPost.vue
+++ src/templates/GhostPost.vue
@@ -41,12 +41,12 @@ export default {
         PostTags
     },
     mounted() {
-        !function (e, n) {
-            for (var o = 0; o < e.length; o++) {
-                var r = n.createElement("script"), c = ".js", d = n.head || n.body;
-                "noModule" in r ? (r.type = "module", c = ".mjs") : r.async = !0, r.defer = !0, r.src = remark_config.host + "/web/" + e[o] + c, d.appendChild(r)
-            }
-        }(remark_config.components || ["embed"], document);
+        insertScript('comments-script', document.body);
+        const remark42 = window.REMARK42;
+        if (remark42) {
+            remark42.destroy();
+            remark42.createInstance(window.remark_config);
+        }
     },
     metaInfo() {
         return {
@@ -60,6 +60,24 @@ export default {
         }
     }
 }
+
+// This function will insert the usual <script> tag of
+// Remark42 into the specified DOM location (parentElement)
+const insertScript = (id, parentElement) => {
+    const script = window.document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.id = id;
+    // Now the actual config and script-fetching function:
+    script.innerHTML = `
+    var remark_config = {
+        host: 'https://remark.maxvamp.com',
+        site_id: 'vampstories',
+        theme: document.documentElement.classList.contains('dark') ? 'dark' : 'light'
+    };
+    !function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);`;
+    parentElement.appendChild(script);
+};
 </script>
 
 <page-query>
